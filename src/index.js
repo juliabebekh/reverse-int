84a38e13d9858d2a9eb9250f27858f91a24bec93
Julia Bebekh <juliabebekh@gmail.com>
2021-03-15 23:36:19 +0300

    init: start the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
-}
+    let resultString = '';
+  let str = '';
+  if (n >= 0) {
+  str = String(n) 
+  } else {
+    str = String(-n)
+  }
+  for (let i = str.length - 1; i >= 0; i--) {
+  resultString += str[i]
+  }
+  return Number(resultString)
+  } 
